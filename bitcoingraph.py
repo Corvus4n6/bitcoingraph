7d95556f38355dc048cd50a20b909b81ea757840
J-Michael Roberts <jroberts@corvusforensics.com>
2023-06-15 15:29:35 -0400

    Add warn option, terminal colors. Prep for future functions.

--- bitcoingraph.py
+++ bitcoingraph.py
@@ -82,7 +82,10 @@ def graphaddress(seedaddress, apikey, timefield, valopts, netstate):
         elif reqjson == False and netstate > 0:
             addrdict = netjson('address', seedaddress, apikey)
         else:
-            sys.exit('Local data for address ' + seedaddress + ' unavailable.')
+            if args.warn:
+                print('Warning: Local data for address ' + seedaddress + ' unavailable.')
+            else:
+                sys.exit('Local data for address ' + seedaddress + ' unavailable. Exiting')
     else:
         addrdict = netjson('address', seedaddress, apikey)
 
@@ -107,7 +110,10 @@ def graphaddress(seedaddress, apikey, timefield, valopts, netstate):
                 elif reqjson == False and netstate > 0:
                     addrdict = netjson('address', addresslist[i], apikey)
                 else:
-                    sys.exit('Local data for address ' + addresslist[i] + ' unavailable.')
+                    if args.warn:
+                        print('Warning: Local data for address ' + seedaddress + ' unavailable.')
+                    else:
+                        sys.exit('Local data for address ' + seedaddress + ' unavailable. Exiting')
             else:
                 addrdict = netjson('address', addresslist[i], apikey)
 
@@ -127,8 +133,10 @@ def graphaddress(seedaddress, apikey, timefield, valopts, netstate):
                 if txdict == False and netstate > 0:
                     txdict = netjson('transaction', transaction, apikey)
                 elif txdict == False and netstate == 0:
-                    # make this a warning?
-                    sys.exit('Local data for transaction ' + transaction + ' unavailable.')
+                    if args.warn:
+                        print('Warning: Local data for address ' + seedaddress + ' unavailable.')
+                    else:
+                        sys.exit('Local data for address ' + seedaddress + ' unavailable. Exiting')
             else:
                 txdict = netjson('transaction', transaction, apikey)
 
@@ -183,124 +191,163 @@ def graphaddress(seedaddress, apikey, timefield, valopts, netstate):
     g.close()
     return
 
-arghelpdesc = 'Bitcoin Transaction Grapher'
-parser = argparse.ArgumentParser(description=arghelpdesc)
-parser.add_argument('-t', '--time', help='Add full transaction timestamps to each arrow on the graph.', action='store_true')
-parser.add_argument('-d', '--date', help='Add date-only transaction timestamps to each arrow on the graph.', action='store_true')
-parser.add_argument('-btc', '--valuebtc', help='Add transaction value in BTC to each arrow on the graph.', action='store_true')
-parser.add_argument('-usd', '--valueusd', help='Add transaction value in USD to each arrow on the graph.', action='store_true')
-parser.add_argument('-f', '--file', help='File to read addresses from. One address per line.', nargs=1)
-parser.add_argument('-l', '--local', help='Attempt to find information in local data folders before making API calls.', action='store_true')
-parser.add_argument('-o', '--offline', help='Only work from information saved in local data folders. No API calls will be made.', action='store_true')
-parser.add_argument('--truncate', help='Truncate Graphviz data to only include transactions with the target address(es).', action='store_true')
-parser.add_argument('--highlight', help='Highlight target address(es) on graph.', action='store_true')
-parser.add_argument('address', help='One or more target address(es) to graph.', nargs=argparse.REMAINDER)
+def graphloop(targetlist):
+    # breaking out for future TODO for option to bypass and just re-concatenate
+    for seedaddress in targetlist:
+        graphaddress(seedaddress, apikey, timefield, valopts, netstate)
+    return
 
-args = parser.parse_args()
-
-if args.time and args.date:
-    sys.exit('ERROR: Options --time and --date are mutually exclusive.')
-if args.local and args.offline:
-    sys.exit('ERROR: Options --local and --offline are mutually exclusive.')
-
-valopts = []
-if args.valuebtc:
-    valopts.append('btc')
-if args.valueusd:
-    valopts.append('usd')
-
-reporttime = datetime.now().strftime('%Y%m%d_%H%M%S')
-
-targetlist=[]
-
-if args.file:
-    try:
-        with open(args.file[0]) as asrcfile:
-            for line in asrcfile:
-                line = line.strip()
-                if line == '':
-                    continue
-                elif re.search('[^0-9a-zA-Z]', line):
-                    print('Skipping possible bad address: ' + line)
-                    continue
-                elif len(line) < 27:
-                    print('Skipping possible bad address: ' + line)
-                    continue
-                if line not in targetlist:
-                    targetlist.append(line)
-    except FileNotFoundError:
-        sys.exit('Address list file ' + args.file[0] + ' not found.')
-
-if args.address:
-    for line in args.address:
-        line = line.strip()
-        if line == '':
-            continue
-        elif re.search('[^0-9a-zA-Z]', line):
-            print('Skipping possible bad address: ' + line)
-            continue
-        elif len(line) < 27:
-            print('Skipping possible bad address: ' + line)
-            continue
-        if line not in targetlist:
-            targetlist.append(line)
-
-if args.time:
-    timefield = 'full'
-elif args.date:
-    timefield = 'date'
-else:
-    timefield = ''
-
-if args.offline:
-    netstate = 0
-elif args.local:
-    netstate = 1
-else:
-    netstate = 2
-
-# get API key from file
-try:
-    with open('apikey.txt', 'r') as k:
-        apikey = k.readline().strip()
-    k.close()
-except FileNotFoundError:
-    sys.exit('Create the file apikey.txt and add your API at the top.')
-
-if len(targetlist) == 0:
-    sys.exit('usage: bitcoingraph.py [--help] [--time] [--date] [--file FILE] [--local] [--offline] [--truncate] [--highlight] [address] [address...]')
-
-for seedaddress in targetlist:
-    graphaddress(seedaddress, apikey, timefield, valopts, netstate)
-
-# concatenate all address graphs into a single graph and/or truncate
-if len(targetlist) > 1 or args.truncate:
-    alltx = []
-    with open('data/'+reporttime+'_graph.dot', 'w') as g:
-        g.write('digraph {\n')
-        if args.highlight:
-            g.write('{\nnode [style=filled]\n')
+def graphconcatenate(targetlist):
+    # concatenate all address graphs into a single graph and/or truncate
+    if len(targetlist) > 1 or args.truncate:
+        alltx = []
+        with open('data/'+reporttime+'_graph.dot', 'w') as g:
+            g.write('digraph {\n')
+            if args.highlight:
+                g.write('{\nnode [style=filled]\n')
+                for address in targetlist:
+                    g.write('"'+address+'" [fillcolor=yellow]\n')
+                g.write('}\n')
+            g.write('rankdir=LR;\n')
             for address in targetlist:
-                g.write('"'+address+'" [fillcolor=yellow]\n')
-            g.write('}\n')
-        g.write('rankdir=LR;\n')
-        for address in targetlist:
-            try:
-                with open('data/'+address+'.dot', 'r') as h:
-                    lines = h.readlines()
-                for line in lines:
-                    if args.truncate:
-                        # only keep transactions with targetlist
-                        txpts = re.findall('[0-9a-zA-Z]+', line)
-                        if len(txpts) < 2:
-                            continue
-                        elif txpts[0] not in targetlist and txpts[1] not in targetlist:
-                            continue
-                    if ' -> ' in line and line not in alltx:
-                        alltx.append(line)
-            except:
-                # catching errors that come up when wallets have no transactions
-                pass
-        for txline in alltx:
-            g.write(txline)
-        g.write('}')
+                try:
+                    with open('data/'+address+'.dot', 'r') as h:
+                        lines = h.readlines()
+                    for line in lines:
+                        if args.truncate:
+                            # only keep transactions with targetlist
+                            txpts = re.findall('[0-9a-zA-Z]+', line)
+                            if len(txpts) < 2:
+                                continue
+                            elif txpts[0] not in targetlist and txpts[1] not in targetlist:
+                                continue
+                        if ' -> ' in line and line not in alltx:
+                            alltx.append(line)
+                except:
+                    # catching errors that come up when wallets have no transactions
+                    pass
+            for txline in alltx:
+                g.write(txline)
+            g.write('}')
+    return
+
+def termcolors():
+    # define the usual terminal colors
+    global trmred
+    global trmgrn
+    global trmcyn
+    global trmyel
+    global trmmag
+    global trmbmag
+    global trmnorm
+    global trmclear
+    # Set the bits we need for outputting to the terminal - CF standard
+    trmred = "\x1B[1m\x1B[31m"  # Bold Red (ANSI) - malware
+    trmgrn = "\x1B[0m\x1B[32m"  # Normal Green (ANSI) - clean
+    trmcyn = "\x1B[1m\x1B[36m"  # Bold Cyan (ANSI)
+    trmyel = "\x1B[0m\x1B[33m"  # Normal Yellow (ANSI) - unknown
+    trmmag = "\x1B[35m"  # Magenta (ANSI)
+    trmbmag = "\x1B[1m\x1B[35m"  # Bold Magenta (ANSI) - errors
+    trmnorm = "\x1B[0m"  # Normal (ANSI) - normal
+    trmclear = "\x1b[2K" # Clear line
+    return
+
+if __name__ == "__main__":
+
+    arghelpdesc = 'Cryptocurrency Transaction Grapher'
+    parser = argparse.ArgumentParser(description=arghelpdesc)
+    parser.add_argument('-t', '--time', help='Add full transaction timestamps to each arrow on the graph.', action='store_true')
+    parser.add_argument('-d', '--date', help='Add date-only transaction timestamps to each arrow on the graph.', action='store_true')
+    parser.add_argument('-btc', '--valuebtc', help='Add transaction value in BTC to each arrow on the graph.', action='store_true')
+    parser.add_argument('-usd', '--valueusd', help='Add transaction value in USD to each arrow on the graph.', action='store_true')
+    parser.add_argument('-a', '--apikey', help='Blockchair API Key.', nargs=1)
+    parser.add_argument('-f', '--file', help='File to read addresses from. One address per line.', nargs=1)
+    parser.add_argument('-l', '--local', help='Attempt to find information in local data folders before making API calls.', action='store_true')
+    parser.add_argument('-o', '--offline', help='Only work from information saved in local data folders. No API calls will be made.', action='store_true')
+    parser.add_argument('-w', '--warn', help='Only warn if missing address and transaction data local folders and proceed anyway.', action='store_true')
+    parser.add_argument('--truncate', help='Truncate Graphviz data to only include transactions with the target address(es).', action='store_true')
+    parser.add_argument('--highlight', help='Highlight target address(es) on graph.', action='store_true')
+    parser.add_argument('address', help='One or more target address(es) to graph.', nargs=argparse.REMAINDER)
+
+    termcolors()
+
+    args = parser.parse_args()
+
+    if args.time and args.date:
+        sys.exit(trmred + 'ERROR: Options --time and --date are mutually exclusive.' + trmnorm)
+    if args.local and args.offline:
+        sys.exit(trmred + 'ERROR: Options --local and --offline are mutually exclusive.' + trmnorm)
+
+    valopts = []
+    if args.valuebtc:
+        valopts.append('btc')
+    if args.valueusd:
+        valopts.append('usd')
+
+    reporttime = datetime.now().strftime('%Y%m%d_%H%M%S')
+
+    targetlist=[]
+
+    if args.file:
+        try:
+            with open(args.file[0]) as asrcfile:
+                for line in asrcfile:
+                    line = line.strip()
+                    if line == '':
+                        continue
+                    elif re.search('[^0-9a-zA-Z]', line):
+                        print(trmyel + 'Skipping possible bad address: ' + line + trmnorm)
+                        continue
+                    elif len(line) < 27:
+                        print(trmyel + 'Skipping possible bad address: ' + line + trmnorm)
+                        continue
+                    if line not in targetlist:
+                        targetlist.append(line)
+        except FileNotFoundError:
+            sys.exit(trmred + 'Address list file ' + args.file[0] + ' not found.' + trmnorm)
+
+    if args.address:
+        for line in args.address:
+            line = line.strip()
+            if line == '':
+                continue
+            elif re.search('[^0-9a-zA-Z]', line):
+                print(trmyel + 'Skipping possible bad address: ' + line + trmnorm)
+                continue
+            elif len(line) < 27:
+                print(trmyel + 'Skipping possible bad address: ' + line + trmnorm)
+                continue
+            if line not in targetlist:
+                targetlist.append(line)
+
+    if args.time:
+        timefield = 'full'
+    elif args.date:
+        timefield = 'date'
+    else:
+        timefield = ''
+
+    if args.offline:
+        netstate = 0
+    elif args.local:
+        netstate = 1
+    else:
+        netstate = 2
+
+    if args.apikey:
+        apikey = args.apikey
+        # sanity check apikey value?
+    else:
+        # get API key from file
+        try:
+            with open('apikey.txt', 'r') as k:
+                apikey = k.readline().strip()
+            k.close()
+        except FileNotFoundError:
+            sys.exit(trmyel + 'Create the file apikey.txt and add your API at the top or specify on the command line with the --apikey option.' + trmnorm)
+
+    if len(targetlist) == 0:
+        sys.exit('usage: bitcoingraph.py [--help] [--time] [--date] [--apikey] [--file FILE] [--local] [--offline] [--truncate] [--highlight] [address] [address...]')
+
+    graphloop(targetlist)
+    graphconcatenate(targetlist)
